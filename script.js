8e2c45c89ac3c845cbabdfe7fae3aca208bbe6ab
Emma Rosenthal <124089752+emmarose-27@users.noreply.github.com>
2024-01-23 18:23:06 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var map = L.map('map').setView([37.7612887808995, -122.4326410508401], 11);
+
+// load a tile layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+$.getJSON('https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson', function(data) {
+  var coordinatesOnly = data.features.map(function(feature) {
+    return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
+  });
+
+  var heat = L.heatLayer(coordinatesOnly).addTo(map);
+});
